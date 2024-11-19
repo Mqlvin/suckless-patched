94d16a81c96a0005871d1aeace480f4e2f196bfd
Mqlvin <pricklydotcactus@gmail.com>
2024-11-19 08:53:45 +0000

    Add dwm-barpadding patch

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows (@fullgaps) */
+static const int vertpad            = 10;       /* vertical padding of bar (@barpadding) */
+static const int sidepad            = 10;       /* horizontal padding of bar (@barpadding) */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
